0b3481951136fa69a1da082dc0992633da1db719
현 <92370007+hy2n@users.noreply.github.com>
2024-07-23 00:53:39 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // 코드1 수행 횟수 출력
+    printf("1\n");
+    
+    // 최고차항의 차수 출력
+    printf("0\n");
+    
+    return 0;
+}
