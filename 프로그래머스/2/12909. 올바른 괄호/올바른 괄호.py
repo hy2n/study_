99ad55afe5d2f94a47262c444e006c85d1d5a8c5
현 <92370007+hy2n@users.noreply.github.com>
2024-09-04 17:41:06 +0900

    [level 2] Title: 올바른 괄호, Time: 14.36 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+"""
+1 . 문자열이 ( 라면 값을 1 추가한다
+2 . 문자열이 ) 라면 값을 1 지운다
+3 . 만약 마지막 코드에서 STACK변수 안에 아무것도 들었으면 TRUE, 아니면 FALSE로..
+"""
+def solution(s):
+    stack = []
+    if(s[0] == ')'):
+        return False
+    for x in range(0,len(s)):
+        if (s[x] == '('):
+            stack.append('(')
+        elif (s[x] == ')') and (len(stack)>0):
+            if(stack[-1] == '('):
+                stack.pop()
+    if (len(stack) == 0):
+        return True
+    else:
+        return False
