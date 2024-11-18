34cdd56d438305662dd1e517f5e65d6b25dfe122
이동현 <github1@ncloud.sbs>
2024-11-18 19:21:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 30.20 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,21 +1,3 @@
-from collections import deque
-
-def solution(n, vertex):
-    graph = {i: [] for i in range(1, n + 1)}
-    for a, b in vertex:
-        graph[a].append(b)
-        graph[b].append(a)
-    
-    distances = [-1] * (n + 1) 
-    distances[1] = 0  
-    queue = deque([1])  
-    
-    while queue:
-        current = queue.popleft()
-        for neighbor in graph[current]:
-            if distances[neighbor] == -1: 
-                distances[neighbor] = distances[current] + 1
-                queue.append(neighbor)
-    
-    max_distance = max(distances)
-    return distances.count(max_distance)
+def solution(n, edge):
+    answer = 0
+    return answer
